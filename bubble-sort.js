d9d723f54446035074ec78d244f842d3ea58d8d7
ksr-nivas <srinivask.ria@gmail.com>
2020-06-23 15:18:34 +0530

    1 file modified

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,7 +1,7 @@
 export default function bubbleSort(array = []) {
   let temp, i, j;
   for (i = 0; i < array.length; i++) {
-    for (j = 1; j < array.length; j++) {
+    for (j = 0; j < array.length; j++) {
       if (array[j] > array[j + 1]) {
         temp = array[j];
         array[j] = array[j + 1];
